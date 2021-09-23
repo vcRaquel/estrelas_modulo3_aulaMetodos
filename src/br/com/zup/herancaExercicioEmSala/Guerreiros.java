95233880e50fe4573cc6798ef0c8e50c41e05f69
Raquel Leão <vc.raquel@gmail.com>
2021-09-23 12:04:40 -0300

    herança exercício em sala estrutura classe

--- src/br/com/zup/herancaExercicioEmSala/Guerreiros.java
+++ src/br/com/zup/herancaExercicioEmSala/Guerreiros.java
@@ -1,4 +1,31 @@
 package br.com.zup.herancaExercicioEmSala;
 
-public class Guerreiros {
+public class Guerreiros extends Heroi{
+    private double poderAtaque;
+    private double aplicarDano;
+
+
+    public Guerreiros(String nome, double vida, double poderAtaque, double aplicarDano) {
+        super(nome, vida);
+        this.poderAtaque = poderAtaque;
+        this.aplicarDano = aplicarDano;
+    }
+
+    public double getPoderAtaque() {
+        return poderAtaque;
+    }
+
+    public void setPoderAtaque(double poderAtaque) {
+        this.poderAtaque = poderAtaque;
+    }
+
+    public double getAplicarDano() {
+        return aplicarDano;
+    }
+
+    public void setAplicarDano(double aplicarDano) {
+        this.aplicarDano = aplicarDano;
+    }
+
+    private double aplicarDano(){}
 }
