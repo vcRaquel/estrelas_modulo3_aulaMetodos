c5f8ab2ca4f7569c2b6730e2be78a70626d53ced
Raquel Leão <vc.raquel@gmail.com>
2021-09-23 12:09:59 -0300

    herança exercício em sala estrutura classe Inimigos

--- src/br/com/zup/herancaExercicioEmSala/Inimigos.java
+++ src/br/com/zup/herancaExercicioEmSala/Inimigos.java
@@ -1,4 +1,31 @@
 package br.com.zup.herancaExercicioEmSala;
 
 public class Inimigos {
+    private String nome;
+    private  double vida;
+
+    public Inimigos(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVida() {
+        return vida;
+    }
+
+    public void setVida(double vida) {
+        this.vida = vida;
+    }
+
+    public void trocarNome(String nome){}
+    public void receberDano(double dano){}
+    public double exibirVida(){}
 }
