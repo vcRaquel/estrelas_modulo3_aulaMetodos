f9e14132c37ab2d8074bc6520e202ccc767c7d01
Raquel Leão <vc.raquel@gmail.com>
2021-09-23 12:00:34 -0300

    herança exercício em sala estrutura classe Magos

--- src/br/com/zup/herancaExercicioEmSala/Magos.java
+++ src/br/com/zup/herancaExercicioEmSala/Magos.java
@@ -1,4 +1,32 @@
 package br.com.zup.herancaExercicioEmSala;
 
-public class Magos {
+public class Magos extends Heroi{
+
+    private double poderMagico;
+    private double aplicarMagia;
+
+
+    public Magos(String nome, double vida, double poderMagico, double aplicarMagia) {
+        super(nome, vida);
+        this.poderMagico = poderMagico;
+        this.aplicarMagia = aplicarMagia;
+    }
+
+    public double getPoderMagico() {
+        return poderMagico;
+    }
+
+    public void setPoderMagico(double poderMagico) {
+        this.poderMagico = poderMagico;
+    }
+
+    public double getAplicarMagia() {
+        return aplicarMagia;
+    }
+
+    public void setAplicarMagia(double aplicarMagia) {
+        this.aplicarMagia = aplicarMagia;
+    }
+
+    private double aplicarMagia(){}
 }
