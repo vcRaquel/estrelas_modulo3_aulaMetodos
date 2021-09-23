398befe9a8f537129261ca8c7c718176acb90c23
Raquel Leão <vc.raquel@gmail.com>
2021-09-23 12:19:51 -0300

    herança exercício em sala estrutura classe Voadores

--- src/br/com/zup/herancaExercicioEmSala/Voadores.java
+++ src/br/com/zup/herancaExercicioEmSala/Voadores.java
@@ -1,4 +1,24 @@
 package br.com.zup.herancaExercicioEmSala;
 
-public class Voadores {
+public class Voadores extends Inimigos{
+    private double danoTiro;
+
+    public Voadores() {
+
+    }
+
+    public Voadores(String nome, double vida, double danoTiro) {
+        super(nome, vida);
+        this.danoTiro = danoTiro;
+    }
+
+    public double getDanoTiro() {
+        return danoTiro;
+    }
+
+    public void setDanoTiro(double danoTiro) {
+        this.danoTiro = danoTiro;
+    }
+
+    private double aplicarDano(){}
 }
